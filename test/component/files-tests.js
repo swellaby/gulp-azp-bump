b264cfa5a25570c4ffce6b0db4d2e056e235b2b4
Caleb Cartwright <caleb.cartwright@outlook.com>
2018-03-22 15:47:19 -0400

    added some additional component tests

--- test/component/files-tests.js
+++ test/component/files-tests.js
@@ -35,42 +35,87 @@ suite('Files Suite:', () => {
         fakeFile = null;
     });
 
-    test('Should bump patch version when nothing is specified', (done) => {
-        const bump = index();
-        bump.once('data', function(newFile) {
-            assert.isNotNull(newFile);
-            assert.isNotNull(newFile.path);
-            assert.deepEqual(JSON.parse(newFile.contents), patchFile);
-            done();
+    suite('Successful bump Suite:', () => {
+        test('Should bump patch version when nothing is specified', (done) => {
+            const bump = index();
+            bump.once('data', function(newFile) {
+                assert.isNotNull(newFile);
+                assert.isNotNull(newFile.path);
+                assert.deepEqual(JSON.parse(newFile.contents), patchFile);
+                done();
+            });
+          
+            bump.write(fakeFile);      
+            bump.end();
         });
-      
-        bump.write(fakeFile);      
-        bump.end();
+    
+        test('Should bump patch version when invalid type is specified', (done) => {
+            const bump = index({ type: 'invalid' });
+            bump.once('data', function(newFile) {
+                assert.isNotNull(newFile);
+                assert.isNotNull(newFile.path);
+                assert.deepEqual(JSON.parse(newFile.contents), patchFile);
+                done();
+            });
+          
+            bump.write(fakeFile);      
+            bump.end();
+        });
+    
+        test('Should bump patch version when patch type is specified', (done) => {
+            const bump = index({ type: helpers.patchReleaseType });
+            bump.once('data', function(newFile) {
+                assert.isNotNull(newFile);
+                assert.isNotNull(newFile.path);
+                assert.deepEqual(JSON.parse(newFile.contents), patchFile);
+                done();
+            });
+          
+            bump.write(fakeFile);      
+            bump.end();
+        });
+    
+        test('Should bump minor version when minor type is specified', (done) => {
+            const bump = index({ type: helpers.minorReleaseType });
+            bump.once('data', function(newFile) {
+                assert.isNotNull(newFile);
+                assert.isNotNull(newFile.path);
+                const minorFile = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'files/minor.json')));
+                assert.deepEqual(JSON.parse(newFile.contents), minorFile);
+                done();
+            });
+          
+            bump.write(fakeFile);      
+            bump.end();
+        });
+    
+        test('Should bump major version when major type is specified', (done) => {
+            const bump = index({ type: helpers.majorReleaseType });
+            bump.once('data', function(newFile) {
+                assert.isNotNull(newFile);
+                assert.isNotNull(newFile.path);
+                const majorFile = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'files/major.json')));
+                assert.deepEqual(JSON.parse(newFile.contents), majorFile);
+                done();
+            });
+          
+            bump.write(fakeFile);      
+            bump.end();
+        });
     });
 
-    test('Should bump patch version when patch type is specified', (done) => {
-        const bump = index({ type: helpers.patchReleaseType });
-        bump.once('data', function(newFile) {
-            assert.isNotNull(newFile);
-            assert.isNotNull(newFile.path);
-            assert.deepEqual(JSON.parse(newFile.contents), patchFile);
-            done();
+    suite('Invalid content Suite:', () => {
+        test('Should bump patch version when nothing is specified', (done) => {
+            const bump = index();
+            bump.once('data', function(newFile) {
+                assert.isNotNull(newFile);
+                assert.isNotNull(newFile.path);
+                assert.deepEqual(JSON.parse(newFile.contents), patchFile);
+                done();
+            });
+          
+            bump.write(fakeFile);      
+            bump.end();
         });
-      
-        bump.write(fakeFile);      
-        bump.end();
-    });
-
-    test('Should bump patch version when invalid type is specified', (done) => {
-        const bump = index({ type: 'invalid' });
-        bump.once('data', function(newFile) {
-            assert.isNotNull(newFile);
-            assert.isNotNull(newFile.path);
-            assert.deepEqual(JSON.parse(newFile.contents), patchFile);
-            done();
-        });
-      
-        bump.write(fakeFile);      
-        bump.end();
     });
 });
